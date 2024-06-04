d1b8e84f3e156292c751fb4b1038d6bcebef3f39
chouyu <chouyu.8ga@gmail.com>
2024-06-04 17:49:05 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -50,7 +50,7 @@ export default defineNuxtConfig({
         vueI18n: './i18n.config.ts',
         strategy: 'no_prefix',
         defaultLocale: 'zh',
-    }
+    },
      nitro: {
          vercel: {
              regions: ["sin1", "syd1", "sfo1", "iad1", "pdx1", "cle1"]
